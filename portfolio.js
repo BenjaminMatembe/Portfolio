4c8683b0aa2c0caf15ca474cb03d663e58d6a1e9
MATA's Mac <matembebenjamin@gmail.com>
2023-04-10 22:31:36 +0300

    Fix eslint errors

--- portfolio.js
+++ portfolio.js
@@ -1,26 +1,24 @@
 const hambergerMenuOpen = document.querySelector(".box");
 
 const menu = document.querySelector(".menuBar");
-const menuLinks = document.querySelectorAll(".menuLinks")
 
-const closeMenu = document.querySelector(".closeMenu")
+const menuLinks = document.querySelectorAll(".menuLinks");
 
+const closeMenu = document.querySelector(".closeMenu");
 
-menuLinks.forEach((link)=>{
-
-link.addEventListener('click', (e)=>{
-    menu.style.display = "none"
-        e.stopPropagation();
-    })
-
-})
-
-hambergerMenuOpen.addEventListener('click', (e)=>{
-    menu.style.display = "block";
+menuLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    menu.style.display = "none";
     e.stopPropagation();
-})
+  });
+});
 
-closeMenu.addEventListener('click', (e)=>{
-        menu.style.display = "none"
-        e.stopPropagation();
-})
+hambergerMenuOpen.addEventListener("click", (e) => {
+  menu.style.display = "block";
+  e.stopPropagation();
+});
+
+closeMenu.addEventListener("click", (e) => {
+  menu.style.display = "none";
+  e.stopPropagation();
+});
